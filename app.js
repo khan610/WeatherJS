b9e834350a8ae491eeee3d0510ff5a92333f999c
Andrea Trajkovic <trajkovicandrea@gmail.com>
2022-09-09 10:40:41 +0200

    Update

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Init weather object
 const weather = new Weather('Boston', 'MA');
 
+// Init UI
+const ui = new UI();
+
 // Get weather on DOM load
 document.addEventListener('DOMcontentLoaded', getWeather);
 
@@ -9,6 +12,6 @@ document.addEventListener('DOMcontentLoaded', getWeather);
 function getWeather() {
   weather
     .getWeather()
-    .then((results) => console.log(results))
+    .then((results) => ui.paint(results))
     .catch((err) => console.log(err));
 }
